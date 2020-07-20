3f0e79f78d753577bff30b7656f23d9383941f03
cristovoxdgm <cristovoxdgm@gmail.com>
2020-07-19 23:01:09 -0300

    estudo sobre slider concluido, aplicando ja os novos conceitos de bare apps

--- App.js
+++ App.js
@@ -1,43 +1,15 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, Picker } from 'react-native';
-
+import Slider from '@react-native-community/slider';
 export default function App() {
 
-  const [pizza, setPizza] = useState(0);
-
-  const flavorsPizza = [
-    { key: 0, nome: "Calabresa", valor: 35.90 },
-    { key: 1, nome: "Frango", valor: 32.90 },
-    { key: 2, nome: "4Queijos", valor: 30.90 },
-  ];
-
-  const [pizzas, setPizzas] = useState(flavorsPizza);
-
-  let pizzasItem = pizzas.map((res, key) => {
-    return <Picker.Item value={res.key} key={key} label={res.nome} />
-  });
+  const [valor, setValor] = useState(100);
 
   return (
     <View style={styles.container}>
 
-      <Text style={styles.logo} >Menu Pizzas</Text>
-
-      <Text style={styles.pizzas} >
-        Você escolheu: {
-          pizzas[pizza].nome
-        }
-      </Text>
-
-      <Text style={styles.pizzas} >R$: {pizzas[pizza].valor.toFixed(1)}</Text>
-      <View style={styles.areaPicker}>
-        <Picker style={{ height: 50, width: 200 }}
-          selectedValue={pizza}
-          onValueChange={(e, index) => setPizza(e)}
-        >
-          {pizzasItem}
-        </Picker>
-      </View>
-
+      <Slider value={valor} minimumValue={0} onValueChange={(value) => setValor(value)} maximumValue={100} style={{ width: 200, height: 50 }} minimumTrackTintColor="green" maximumTrackTintColor="red" />
+      <Text>{valor.toFixed(0)}</Text>
     </View>
   );
 
@@ -47,23 +19,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    marginTop: 20
-  },
-  logo: {
-    textAlign: "center",
-    fontSize: 28,
-
-  },
-  pizzas: {
-    marginTop: 15,
-    fontSize: 28,
-    textAlign: "center"
-  },
-  areaPicker: {
-    marginTop: 10,
+    marginTop: 20,
     alignItems: "center",
     justifyContent: "center"
-  }
+  },
+
 });
 
 
